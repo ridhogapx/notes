6c2b7f82148a00e41a83d77e3b85fe6f5d649272
RageNeko26 <rneko2006@gmail.com>
2023-11-04 00:52:06 +0700

    fix: Add static controller

--- controller/notes_controller.go
+++ controller/notes_controller.go
@@ -13,5 +13,5 @@ func NewNotesController(r *gin.Engine) *notesController {
 }
 
 func (controller *notesController) Home() {
-  controller.R.Static("/", "./view")
+  controller.R.Static("/", "./view") 
 }
