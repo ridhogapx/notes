50cdfad7a8339066e46095be39691ac9b35a0fdd
RageNeko26 <rneko2006@gmail.com>
2023-11-06 10:12:44 +0700

    fix: Mock object naming variable

--- model/note_model_mock.go
+++ model/note_model_mock.go
@@ -9,15 +9,15 @@ type MockNoteModel struct {
   mock.Mock
 }
 
-func (m *MockNoteModel) CreateNote(data *Notes) error {
+func (m *MockNoteModel) CreateNote(data *Note) error {
   arg := m.Called(data)
 
   return arg.Error(0)
 }
 
-func (m *MockNoteModel) FindNote(id string) (*Notes, error) {
+func (m *MockNoteModel) FindNote(id string) (*Note, error) {
   arg := m.Called(id)
   
-  res := arg.Get(0).(Notes)
+  res := arg.Get(0).(Note)
   return &res, arg.Error(0)
 }  
