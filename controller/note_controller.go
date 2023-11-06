d1ee662b3d4f5ff8ae9ed896b9c65871ff048fd0
RageNeko26 <rneko2006@gmail.com>
2023-11-06 09:44:47 +0700

    refactor: Change stub method

--- controller/note_controller.go
+++ controller/note_controller.go
@@ -2,27 +2,28 @@ package controller
 
 import (
 	"net/http"
+	"notes-v1/model"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
-type notesController struct {
-	R *gin.Engine
+type NoteController struct {
+  Model model.NoteModel
 }
 
-func NewNotesController(r *gin.Engine) *notesController {
-  return &notesController{
-    R: r,
+func NewNoteController(model model.NoteModel) *NoteController {
+  return &NoteController{
+    Model: model,
   }
 }
 
-func (controller *notesController) Init() {
-  controller.R.Use(static.Serve("/", static.LocalFile("view/", true)))
-  controller.R.GET("/api/foo", controller.Ping)
+func (controller *NoteController) Route(app *gin.Engine) {
+  app.Use(static.Serve("/", static.LocalFile("view/", true)))
+  app.GET("/api/foo", controller.Ping)
 }
 
-func (controller *notesController) Ping(ctx *gin.Context) {
+func (controller *NoteController) Ping(ctx *gin.Context) {
   ctx.JSON(http.StatusOK, gin.H{
     "message": "Bar",
   })
