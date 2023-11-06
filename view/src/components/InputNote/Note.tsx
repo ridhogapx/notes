c4ff0a43399edd2d92995836c6a7fde535831000
RageNeko26 <rneko2006@gmail.com>
2023-11-06 17:10:57 +0700

    feat: Improve styling for Save Button

--- view/src/components/InputNote/Note.tsx
+++ view/src/components/InputNote/Note.tsx
@@ -1,11 +1,14 @@
 import NoteTitle from "./Title"
 import NoteBody from "./Body"
+import SaveNote from "./Save"
 
 const NoteInput = () => {
    return (
      <div className="input-container">
       <NoteTitle />
       <NoteBody />
+      <SaveNote />
+      <div className="clear"></div>
     </div>
    )
  }
---
+++ view/src/components/InputNote/Save.tsx
@@ -0,0 +1,7 @@
+const SaveNote = () => {
+  return (
+    <button className="save-note">Save</button>
+  )
+}
+
+export default SaveNote
