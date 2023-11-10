551c0c083777662a935440ccb61331622d11984f
RageNeko26 <rneko2006@gmail.com>
2023-11-10 10:59:28 +0700

    fix: Change context implementation in db instance

--- config/db.go
+++ config/db.go
@@ -14,17 +14,17 @@ func NewDBConn(source string) *gorm.DB {
 	db, err := gorm.Open(postgres.Open(source), &gorm.Config{})
 
   // Setup context time out
-  ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+  ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
   defer cancel()
 
-  tx := db.WithContext(ctx)
+  db.WithContext(ctx)
 
   if err != nil {
     fmt.Println("Failed to connect database", err)
     return nil
   }
  
-  tx.AutoMigrate(&model.Note{})
+  db.AutoMigrate(&model.Note{})
 
-  return tx
+  return db
 }
