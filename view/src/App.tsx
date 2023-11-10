6b5ed20cfc1953373917749f3b97114668a2bc01
RageNeko26 <rneko2006@gmail.com>
2023-11-10 12:59:36 +0700

    feat: Calls HTTP Network to retrieve note data

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/public/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Navbar from "./components/Navbar/Navbar"
 import NoteInput from "./components/InputNote/Note"
 
-function App() { 
+function App() {
+  useEffect(() => {
+      axios.get("/api/v1/notes").then((res) => {
+          console.log(res.data.message)
+        })
+    }, [])
+
   return (
     <>
      <Navbar /> 
