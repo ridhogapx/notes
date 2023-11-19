f44a49c003075d668bce6ae4581623bba235c285
RageNeko26 <rneko2006@gmail.com>
2023-11-19 23:30:48 +0700

    feat: Setup test route

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-  fmt.Println("Notes app")
+  r := gin.Default()
+  r.GET("/", func(ctx *gin.Context) {
+    ctx.JSON(200, gin.H{
+      "message" : "hello world",
+    })
+  })
+
+  r.Run(":3000")
 }
