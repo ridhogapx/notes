7c4a456c6f5ae59f993c91d5442c9bc51dda8214
RageNeko26 <rneko2006@gmail.com>
2023-11-21 14:17:49 +0700

    feat: Setup instance of repos in controller

--- controller/auth_controller.go
+++ controller/auth_controller.go
@@ -1,2 +1,14 @@
-package controller 
+package controller
+
+import "notes/repository"
+
+type AuthController struct {
+  Repos repository.AuthRepository
+} 
+
+func NewAuthController(repos repository.AuthRepository) *AuthController {
+  return &AuthController{
+    Repos: repos,
+  }
+}
 
