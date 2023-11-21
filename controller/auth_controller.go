c9bc5e41bb52f2606c6b052f590da729a19eaa81
RageNeko26 <rneko2006@gmail.com>
2023-11-21 22:45:09 +0700

    feat: Add static css and js

--- controller/auth_controller.go
+++ controller/auth_controller.go
@@ -11,6 +11,11 @@ type AuthController struct {
   Repos repository.AuthRepository
 } 
 
+const (
+  BootstrapCSS = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+  BootstrapJS = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+)
+
 func NewAuthController(repos repository.AuthRepository) *AuthController {
   return &AuthController{
     Repos: repos,
@@ -25,5 +30,7 @@ func (controller *AuthController) Routes(r *gin.Engine) {
 func (controller *AuthController) SignUp(ctx *gin.Context) {
   ctx.HTML(http.StatusOK, "signup.html", gin.H{
     "title" : "Sign Up - Notes",
+    "style" : BootstrapCSS,
+    "script": BootstrapJS,
   })
 }
