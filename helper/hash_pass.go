fb475d3c5ddb0f4b7059f6d6595956c9424d0c38
RageNeko26 <rneko2006@gmail.com>
2023-11-23 16:51:07 +0700

    feat: Add helper for hashing password

---
+++ helper/hash_pass.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(pass string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), 14)
+
+	if err != nil {
+		fmt.Println("Failed to hashing password:", err)
+		return ""
+	}
+
+	return string(hash)
+}
