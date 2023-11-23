e6529cbbf5db643eb1c3ee553389fc9b0b113a34
RageNeko26 <rneko2006@gmail.com>
2023-11-23 19:00:10 +0700

    test: Should be hashed password

---
+++ helper/hash_pass_test.go
@@ -0,0 +1,13 @@
+package helper
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHash(t *testing.T) {
+	hash := HashPassword("myPass")
+
+	fmt.Println("Password:", hash)
+	fmt.Println("Length:", len(hash))
+}
