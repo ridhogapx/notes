0701346448504448d6d2a1cd557f6f0feb673fef
RageNeko26 <rneko2006@gmail.com>
2023-11-23 16:20:44 +0700

    feat: Add auth request payload

--- model/user.go
+++ model/user.go
@@ -1,9 +1,14 @@
-package model 
+package model
 
 type User struct {
-  ID string
-  Email string 
-  Password string 
-  Name string 
-  Role uint8 
+	ID       string
+	Email    string
+	Password string
+	Name     string
+	Role     uint8
+}
+
+type AuthRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
