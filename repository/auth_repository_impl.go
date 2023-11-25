be932a63f83cb2974df2440b9682acb3656655bc
RageNeko26 <rneko2006@gmail.com>
2023-11-25 18:28:08 +0700

    fix: Returned rows affected

--- repository/auth_repository_impl.go
+++ repository/auth_repository_impl.go
@@ -1,39 +1,40 @@
 package repository
 
 import (
+	"errors"
 	"notes/model"
 
 	"gorm.io/gorm"
 )
 
 type authRepositoryImpl struct {
-  q *gorm.DB
+	q *gorm.DB
 }
 
 func NewAuthRepository(db *gorm.DB) AuthRepository {
-  return &authRepositoryImpl{
-    q: db,
-  }
+	return &authRepositoryImpl{
+		q: db,
+	}
 }
 
 func (repo *authRepositoryImpl) CreateUser(user *model.User) error {
-  exec := repo.q.Create(user)
-  
-  if exec.Error != nil {
-    return exec.Error
-  }
+	exec := repo.q.Create(user)
 
-  return nil
+	if exec.Error != nil {
+		return exec.Error
+	}
+
+	return nil
 }
 
 func (repo *authRepositoryImpl) FindUser(email string) (*model.User, error) {
-  var user model.User
+	var user model.User
 
-  exec := repo.q.Where("email = ?", email).First(&user)
-  
-  if exec.Error != nil {
-    return nil, exec.Error
-  }
+	exec := repo.q.Where("email = ?", email).First(&user)
 
-  return &user, nil
+	if exec.RowsAffected == 0 {
+		return nil, errors.New("record is not found")
+	}
+
+	return &user, nil
 }
